96d7316149ecdb76ac025dec83fe6fdeb7f75964
robertobastone <robertobastone93@gmail.com>
2021-03-27 19:33:33 +0100

    v 1.00

--- sol.py
+++ sol.py
@@ -1,10 +1,11 @@
 ######################### LIBRARIES #########################
-import requests # CALL GET
-import re
+import requests # manage rest api
+import string # working with strings
+import re # more features involving strings
 
 ##### hardcoded values
-expected_lyrics_lenght = 140
-range_lyrics_lenght = 10
+expected_lyrics_length = 140
+range_lyrics_length = 10
 
 ##### GENERATING GET REQUEST
 base_url = 'https://api.lyrics.ovh/v1/'
@@ -30,13 +31,48 @@ def getLyrics():
     except Exception as e:
         print("getLyrics - The following exception was catched: " + str(e))
 
+def getArtistAndSong():
+    try:
+        artist = string.capwords(band.replace('/',''))
+        songTitle = string.capwords(song)
+        return artist + ' - ' + songTitle
+    except Exception as e:
+        print("getSongAndArtist - The following exception was catched: " + str(e))
 
+def getHashtags():
+    return '#paroledechanson #lyrics'
+
+def callTwitter(main_message):
+    ##### GENERATING TWITTER REQUEST
+    # Credentials
+    CONSUMER_KEY = environ['TWITTER_CONSUMER_KEY']
+    CONSUMER_SECRET = environ['TWITTER_CONSUMER_SECRET']
+    ACCESS_KEY = environ['TWIITER_ACCESS_KEY']
+    ACCESS_SECRET = environ['TWITTER_ACCESS_SECRET']
+    # Authenticate to Twitter: via environment variables
+    auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY,ACCESS_SECRET)
+    api = tweepy.API(auth)
+    try:
+        if api.verify_credentials() == False:
+            print("The user credentials are invalid.")
+        else:
+            print("The user credentials are valid.")
+            api.update_status(main_message + thanking_message)
+    except tweepy.error.TweepError as e:
+        print("callTwitter - The following exception was catched: " + str(e))
+        errorcode = str(e.api_code)
+        print("Error code: " + errorcode)
+    except Exception as e:
+        print("callTwitter - The following exception was catched: " + str(e))
+        print("Breaking from loop. Better luck next time.")
 
 ####### main method
 try:
     lyrica = getLyrics()
-    song = getSong()
-    print(lyrica)
-    print(song)
+    authorAndSong = getArtistAndSong()
+    hashtags = getHashtags()
+    message = lyrica + '\n' + authorAndSong + '\n' + hashtags
+    print(message)
 except Exception as e:
     print("main - The following exception was catched: " + str(e))
