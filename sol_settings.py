946b8ce474d9019c67e329437439439f2b3a9920
Roberto Bastone <35118511+robertobastone@users.noreply.github.com>
2021-04-11 09:54:33 +0200

    Update sol_settings.py
    King crimson

--- sol_settings.py
+++ sol_settings.py
@@ -7,8 +7,8 @@ max_lyrics_length = 200
 
 ##### 1ST GET REQUEST PARAMETERS
 base_url = 'https://api.lyrics.ovh/v1/'
-band = 'kasabian' + '/'
-song = 'where did all the love go?' # if a song starts with 'the', write it as 'The'
+band = 'king crimson' + '/'
+song = 'moon child' # if a song starts with 'the', write it as 'The'
 
 ##### 2ND GET REQUEST PARAMETERS
 list_words_to_keep_lowercase = ['of','the','with','on','for','a','in']
