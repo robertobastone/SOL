31c19ae5155a503000cf0d1ad16aa85a9f422e1b
Roberto Bastone <35118511+robertobastone@users.noreply.github.com>
2021-04-15 23:34:22 +0200

    Update sol_settings.py
    The verve

--- sol_settings.py
+++ sol_settings.py
@@ -7,8 +7,8 @@ max_lyrics_length = 200
 
 ##### 1ST GET REQUEST PARAMETERS
 base_url = 'https://api.lyrics.ovh/v1/'
-band = 'king crimson' + '/'
-song = 'moonchild' # if a song starts with 'the', write it as 'The'
+band = 'The verve' + '/'
+song = 'bitter sweet symphony' # if a song starts with 'the', write it as 'The'
 
 ##### 2ND GET REQUEST PARAMETERS
 list_words_to_keep_lowercase = ['of','the','with','on','for','a','in']
